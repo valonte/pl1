05516330be8b61423745df50c78e5b09eebe42d0
valonte <91283898+valonte@users.noreply.github.com>
2022-02-01 14:01:33 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 5,
     center: { lat: 51.292, lng: -86.828 },
   });
   // Create an array of alphabetical characters used to label the markers.
